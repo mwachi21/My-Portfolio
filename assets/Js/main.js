bf9010c10eb0cb851ea12ac9fb4c99ad3435f65c
mwachi21 <mwachidennis567@gmail.com>
2023-06-15 00:53:10 +0300

    Progress

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -5,3 +5,14 @@ window.addEventListener("scroll", function() {
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0)
  })
+
+
+//  scroll
+const boxes = document.querySelectorAll('div');
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes(){
+    console.log(window.innerHeight);
+}
